d41a504ed17046132beea595c8ecc0892769cc90
Henry Stanley <henry@henrystanley.com>
2014-10-01 14:44:24 +0100

    Updated layouts, added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.destroy_all
+Review.destroy_all
+
+kfc = Restaurant.create(:name => "KFC")
+kfc.reviews.create(rating: 1, content: "vile")
+kfc.reviews.create(rating: 2, content: "nice")
+kfc.reviews.create(rating: 2, content: "fucked up")
+kfc.reviews.create(rating: 4, content: "extraordinary")
+kfc.reviews.create(rating: 3, content: "gross")
+
+jamies = Restaurant.create(:name => "Jamie's Italian")
+jamies.reviews.create(rating: 1, content: "awful")
+jamies.reviews.create(rating: 2, content: "he swore at me")
+jamies.reviews.create(rating: 2, content: "why is his tongue like that")
+jamies.reviews.create(rating: 4, content: "inedible")
+jamies.reviews.create(rating: 3, content: "help me")
