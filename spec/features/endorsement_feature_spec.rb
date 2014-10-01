2f03df9c2704e64d5d69febfa24dfd5c82c30916
Henry Stanley <henry@henrystanley.com>
2014-10-01 14:53:08 +0100

    Ran rails g controller endorsements, added link to endorse on restaurant view page

--- spec/features/endorsement_feature_spec.rb
+++ spec/features/endorsement_feature_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'endorsing reviews' do
   before do
-    kfc = Restaurant.create(name: 'KFC')
-    kfc.reviews.create(rating: 3, content: "It was an abomination")
+    @kfc = Restaurant.create(name: 'KFC')
+    @kfc.reviews.create(rating: 3, content: "It was an abomination")
   end
 
   it 'a user can endorse a review, which updates the review endorsement count' do
-    visit '/restaurants'
+    visit "/restaurants/#{@kfc.id}"
     click_link 'Endorse'
     expect(page).to have_content('1 endorsement')
   end
