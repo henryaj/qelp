c9dec1e4e4aeb1ab4fbee85286af3cc83ad13172
Henry Stanley <henry@henrystanley.com>
2014-10-01 14:56:06 +0100

    Updated controller and added has_many to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
-
+  has_many :endorsements
   validates :rating, inclusion: { in: [1,2,3,4,5], message: "Your rating must be between 1 to 5."}
 end
