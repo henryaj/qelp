8225ac5c92627306d1f34eb8bda6c39ce21573f0
Henry Stanley <henry@henrystanley.com>
2014-10-01 14:49:19 +0100

    Added test for review endorsements and new route

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resources :reviews do
+    resources :endorsements
+  end
+
   # Example resource route with options:
   #   resources :products do
   #     member do
---
+++ spec/features/endorsement_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'endorsing reviews' do
+  before do
+    kfc = Restaurant.create(name: 'KFC')
+    kfc.reviews.create(rating: 3, content: "It was an abomination")
+  end
+
+  it 'a user can endorse a review, which updates the review endorsement count' do
+    visit '/restaurants'
+    click_link 'Endorse'
+    expect(page).to have_content('1 endorsement')
+  end
+  
+end
